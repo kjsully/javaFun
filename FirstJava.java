a0abe6da7850f01626fd536076642bd1734e6ea4
Kyle Sullivan <kylesullivan123@gmail.com>
2021-07-31 18:27:16 -0700

    commit2

---
+++ FirstJava.java
@@ -0,0 +1,7 @@
+public class FirstJava {
+    public static void main(String[] args) {
+        System.out.println("My name is Kyle Sullivan");
+        System.out.println("I am 28 years old");
+        System.out.println("My hometown is Seattle, WA");
+    }
+}
